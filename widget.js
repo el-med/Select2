3e4f67d2db41d11c31bab648f1f244edaa40f41a
m.elharrak <med.elharrak@gmail.com>
2014-04-25 17:36:02 +0100

    adapt widget to Wak9

--- widget.js
+++ widget.js
@@ -1,4 +1,4 @@
-/*global WAF, $, ds */
+/*global WAF, $, ds, sources, window */
 
 'use strict';
 
@@ -11,18 +11,25 @@ WAF.define('Select2', ['waf-core/widget'], function (widget) {
             bindable : true
         }),
 
+        autoSave: widget.property({
+            type: 'boolean',
+            defaultValue : true,
+            bindable : false
+        }),
+
         items: widget.property({
             type: 'datasource',
             attributes: [{
                 name: 'id' // Option key
             }, {
                 name: 'text' // Option displayed text
-            }]
+            }],
+            onChange : function () {
+                this.main();
+            }
         }),
 
         init: function () {
-            var self = this;
-
             this.node.innerHTML = '<input />';
             this.$selectNode = $('input', this.node);
 
@@ -32,11 +39,45 @@ WAF.define('Select2', ['waf-core/widget'], function (widget) {
                 width: '100%'
             });
 
-            // If the collection of the datasource items change, the elements in the DropDownMenu change accordingly.
-            this.items.onCollectionChange(function (elements) {
-                var currentValue;
+            if (window.designer) {
+                this.$selectNode.select2("enable", false);
+            } else {
+                this.main();
+            }
+        },
 
-                currentValue = this.value();
+        main: function () {
+            var self = this,
+                isRelated,
+                dc,
+                re,
+                relatedDC,
+                kind,
+                id = self.items.attributeFor('id'),
+                val = self.value.boundDatasource();
+
+            if (val) {
+                dc = val.datasource.getID();
+                re = val.attribute;
+                relatedDC = self.items().getClassTitle();
+                if (val.datasource[re]) {
+                    kind = val.datasource[re].emAtt ? val.datasource[re].emAtt.kind : '';
+                } else {
+                    kind = '';
+                }
+            }
+
+            if (kind === "relatedEntity") {
+                isRelated = true;
+            } else {
+                isRelated = false;
+            }
+
+
+            // If the collection of the datasource items change, 
+            // the elements in the DropDownMenu change accordingly.
+            // this.items.onCollectionChange(function (elements) {
+            self.items.onPageChange(function (elements) {
 
                 // Check to see if there is any element on the new collection
                 if (!elements.length) {
@@ -49,46 +90,123 @@ WAF.define('Select2', ['waf-core/widget'], function (widget) {
                     data: elements
                 });
 
-                // Initialize the combobox with the current value
-                if (currentValue) {
-                    this.$selectNode.select2("val", currentValue);
+                //Initialize the combobox with the current value
+                if (!isRelated) {
+                    self.$selectNode.select2("val", self.value());
                 }
             });
 
-            // If the value of the attribute countryCode of the datasource person1 change, 
-            // the selected element in the DropDownMenu change
-            this.value.onChange(function () {
-                var currentValue, key, dataClass;
+            // When the current element of the related datasource "company" is changed, 
+            // 1. the selected element of the DropDownMenu change 
+            // 2. the value of the related attribute "employee.company" is changed too.
+            if (isRelated) {
+                if (self.items()) {
+                    //this.items().subscribe('currentElementChange', function (event) {
+                    self.items.subscribe('currentElementChange', function (event) {
 
-                currentValue = this.value();
-                key = this.items.attributeFor('id');
-                dataClass = this.items().getClassTitle();
-                ds[dataClass].find(key + ' LIKE ' + currentValue, {
+                        var currentEntity = event.data.dataSource.getCurrentElement();
+                        if (currentEntity) {
+                            self.$selectNode.select2("val", currentEntity[id].getValue());
+                            sources[dc][re].set(currentEntity);
+                            if (self.autoSave()) {
+                                sources[dc].save();
+                            }
+                        }
+                    });
+                }
+            }
+
+
+            // When the related attribute "employee.company" is changed, 
+            // 1. then the chosen element in the DropDownMenu is changed, 
+            // 2. the current element of the related datasource "company" is changed too.            
+            self.value.onChange(function (newVal) {
+                if (isRelated) {
+                    onValueChangeRelated(newVal);
+                } else {
+                    onValueChangeSimple();
+                }                
+            });
+
+            function onValueChangeRelated(newVal) {
+                if (newVal && newVal.__KEY) {
+                    ds[relatedDC].query(id + ' = :1', {
+                        onSuccess: function (event) {
+                            event.result.getEntity(0, {
+                                onSuccess: function (result) {
+                                    self.items().selectByKey(result.entity.getKey());
+                                    self.$selectNode.select2("val", result.entity[id].getValue());
+                                }
+                            });
+                        },
+                        params: [newVal.__KEY]
+                    });
+                }
+            }
+
+            function onValueChangeSimple() {
+                var currentValue, key, dataClass, query;
+
+                currentValue = self.value();
+                key = self.items.attributeFor('id');
+                dataClass = self.items().getClassTitle();
+                if (typeof currentValue === "number") {
+                    query = key + ' LIKE ' + currentValue;
+                } else {
+                    query = key + ' LIKE "' + currentValue + '"';
+                }
+                
+                ds[dataClass].find(query, {
                     'onSuccess': function (e) {
                         if (e.entity) {
                             self.$selectNode.select2("val", currentValue);
                         } else {
-                            //@TODO: if no element match the value of the attribute, the widget should raise an error
+                            // show empty option if no value is matched
+                            self.$selectNode.select2("val", null);
                             return;
                         }
                     }
                 });
-            });
+            }
 
-            // When the user select another item in the DropDownMenu, the value of the attribute "id" of 
-            // the selected item is copied to the attribute value of the datasource value.
-            this.$selectNode.on({
+            self.$selectNode.on({
                 "change": function (e) {
-                    var bindedValue = self.boundAttributes.value;
-                    if (bindedValue) {
-                        bindedValue.datasource[bindedValue.attribute] = this.value;
-                        bindedValue.datasource.save();
-                        self.fire("change", e.data);
+                    if (isRelated) {
+                        onSelectNodeRelated(e);
                     } else {
-                        self.value(this.value);
+                        onSelectNodeSimple(e);
                     }
                 }
             });
+
+            // When the user select another item in the DropDownMenu
+            // 1.  the related attribute employee.company is set to the corresponding company item
+            // 2.  the current element of the related datasource "company" is changed to this item
+            function onSelectNodeRelated(e) {
+                ds[relatedDC].query(id + ' = :1', {
+                    onSuccess: function (event) {
+                        event.result.getEntity(0, {
+                            onSuccess: function (result) {
+                                self.items().selectByKey(result.entity.getKey());
+                            }
+                        });
+                    },
+                    params: [e.val]
+                });
+            }
+
+            // When the user select another item in the DropDownMenu, the value of the attribute "id" of 
+            // the selected item is copied to the attribute value of the datasource value.            
+            function onSelectNodeSimple(e) {
+                if (val) {
+                    val.datasource[val.attribute] = e.val;
+                    if (self.autoSave()) {
+                        val.datasource.save();
+                    }
+                } else {
+                    self.value(e.val);
+                }
+            }
         }
     });
 
